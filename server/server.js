64b31fc43d0116b6acf6ef472e1ff33aa1a72c88
rajeshparmar69576 <rp69576@gmail.com>
2025-01-30 21:29:55 +0530

    created model for todo page

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 import express from 'express'
 import cors from 'cors'
+import connDB from './config/db.config.js'
 
 const app = express()
 const PORT = process.env.PORT || 1001
@@ -14,6 +15,8 @@ app.use(express.json())
 // app listening
 app.listen(PORT,()=>{
     console.log(`Server is running on http://localhost:${PORT}/`)
+    connDB()
+
 })
 
 
